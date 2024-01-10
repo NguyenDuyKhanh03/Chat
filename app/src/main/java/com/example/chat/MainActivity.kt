f6def5150b310ab3fdcef21b7e0a12329b7234e4
Nguyen Duy Khanh <duykhanhz112233@gmail.com>
2024-01-10 21:44:51 +0700

    Cập nhật đăng xuất và trở về SignInActivity

--- app/src/main/java/com/example/chat/MainActivity.kt
+++ app/src/main/java/com/example/chat/MainActivity.kt
@@ -1,11 +1,74 @@
 package com.example.chat
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.chat.databinding.ActivityMainBinding
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bingding:ActivityMainBinding
+    private lateinit var auth:FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bingding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bingding.root)
+        auth=FirebaseAuth.getInstance()
+        bingding.topAppBar.setOnMenuItemClickListener { item ->
+            when(item.itemId){
+                R.id.settings-> {
+                    Toast.makeText(this,"Setting is clicked",Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.groupChat-> {
+                    Toast.makeText(this,"Group chat is started",Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.logout-> {
+                    auth.signOut()
+                    val intent= Intent(this,SignInActivity::class.java)
+                    startActivity(intent)
+                    Toast.makeText(this,"Logout",Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else->
+                    false
+            }
+        }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        Log.d("Menu", "onCreateOptionsMenu is called.")
+        val inflater:MenuInflater=menuInflater
+        inflater.inflate(R.menu.menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        Log.d("Menu", "onOptionsItemSelected is called.")
+        when(item.itemId){
+            R.id.settings-> {
+                Toast.makeText(this,"Setting is clicked",Toast.LENGTH_SHORT).show()
+            }
+            R.id.groupChat-> {
+                Toast.makeText(this,"Group chat is started",Toast.LENGTH_SHORT).show()
+            }
+            R.id.logout-> {
+                auth.signOut()
+                Toast.makeText(this,"Logout",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+
+
 }
+
